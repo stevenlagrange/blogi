5e06eb7ad6590169749991024be141dcccf667c9
Steven Lagrange <slagrang@ualberta.ca>
2024-09-11 04:24:16 -0600

    Update relative import

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -99,7 +99,7 @@ const siteConfig: SiteConfig = {
         title: 'Hi There & Welcome to My Corner of the Web!',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
-            src: '/hero.jpeg',
+            src: 'blogi/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
         },
         actions: [
