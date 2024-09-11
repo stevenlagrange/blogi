ca5a6f22b7cbc1eb92e544604e8fa36880cb01b6
Steven Lagrange <slagrang@ualberta.ca>
2024-09-11 04:31:48 -0600

    Adding base relative imports for public hero image

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -42,25 +42,25 @@ const siteConfig: SiteConfig = {
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
-        src: '/dante-preview.jpg',
+        src: '/blogi/dante-preview.jpg',
         alt: 'Dante - Astro.js and Tailwind CSS theme'
     },
     headerNavLinks: [
         {
             text: 'Home',
-            href: '/'
+            href: '/blogi'
         },
         {
             text: 'Projects',
-            href: '/projects'
+            href: '/blogi/projects'
         },
         {
             text: 'Blog',
-            href: '/blog'
+            href: '/blogi/blog'
         },
         {
             text: 'Tags',
-            href: '/tags'
+            href: '/blogi/tags'
         }
     ],
     footerNavLinks: [
@@ -99,7 +99,7 @@ const siteConfig: SiteConfig = {
         title: 'Hi There & Welcome to My Corner of the Web!',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
-            src: 'blogi/hero.jpeg',
+            src: '/blogi/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
         },
         actions: [
